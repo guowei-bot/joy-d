7969263d902698ecd9236617013e7077df113e03
guowezhou01 <guowezhou01.com>
2023-07-25 23:42:58 +0800

    t

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,8 +3,8 @@ export async function signInUseSupabase({ supabase }) {
     if(process.env.NEXT_PUBLIC_SANDBOX=='true') {
         redirectTo = 'http://localhost:3000/apps'
     } else {
-       // redirectTo = 'https://go-sea-template.vercel.app/apps'
-       redirectTo = 'http://zhouguowei.com/apps'
+        redirectTo = 'https://joy_demo.vercel.app/apps'
+       //redirectTo = 'http://zhouguowei.com/apps'
     }
 
     const { data, error } = await supabase.auth.signInWithOAuth({
