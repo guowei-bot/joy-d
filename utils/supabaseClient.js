d8c229723ca9bb8dd8450abe7d49ea7762830981
guowezhou01 <guowezhou01.com>
2023-07-26 00:41:14 +0800

    改配置

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,8 +1,8 @@
 export async function signInUseSupabase({ supabase }) {
     let redirectTo;
     if(process.env.NEXT_PUBLIC_SANDBOX=='true') {
-        //redirectTo = 'http://localhost:3000/apps'
-        redirectTo = 'https://joy-d.vercel.app/apps'
+        redirectTo = 'http://localhost:3000/apps'
+        //redirectTo = 'https://joy-d.vercel.app/apps'
     } else {
         redirectTo = 'https://joy-d.vercel.app/apps'
        //redirectTo = 'http://zhouguowei.com/apps'
